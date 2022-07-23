9cbc7e2f65462ef867118e01fac1db111e021f72
CodexpathCommunity <codexpath3@gmail.com>
2022-07-23 16:55:57 +0100

    style: add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { GlobalContainer } from '../styles/global';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GlobalContainer>
+      {/* header */}
+      <Component {...pageProps} />
+      {/* footer */}
+    </GlobalContainer>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/global.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const GlobalContainer = styled.div`
+  display: grid;
+  min-height: 100vh;
+  grid-template-rows: auto 1fr auto;
+  position: relative;
+`;
