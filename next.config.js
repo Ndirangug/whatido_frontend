8296144726f1a271b3f2041dd4a32737e8aa3b0d
CodexpathCommunity <codexpath3@gmail.com>
2022-09-27 19:54:49 +0100

    build: mui config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,29 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
-module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['donnysliststory.sfo3.cdn.digitaloceanspaces.com'],
-  },
-});
+
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+]);
+
+module.exports = withBundleAnalyzer(
+  withTM({
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+      domains: ['donnysliststory.sfo3.cdn.digitaloceanspaces.com'],
+    },
+    experimental: {
+      // Enables the styled-components SWC transform
+      styledComponents: true,
+    },
+    webpack: (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@mui/styled-engine': '@mui/styled-engine-sc',
+      };
+      return config;
+    },
+  })
+);
