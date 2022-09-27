55e85fd607e80571687b587573164a658e03cca1
CodexpathCommunity <codexpath3@gmail.com>
2022-09-27 20:03:15 +0100

    build: add mui config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,21 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const withTM = require('next-transpile-modules')([
   '@mui/material',
   '@mui/system',
 ]);
 
-module.exports = withBundleAnalyzer(
-  withTM({
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ['donnysliststory.sfo3.cdn.digitaloceanspaces.com'],
-    },
-    experimental: {
-      // Enables the styled-components SWC transform
-      styledComponents: true,
-    },
-    webpack: (config) => {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@mui/styled-engine': '@mui/styled-engine-sc',
-      };
-      return config;
-    },
-  })
-);
+module.exports = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['donnysliststory.sfo3.cdn.digitaloceanspaces.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    };
+    return config;
+  },
+});
