5c158e75ae1b75faed59277108892978ee23f1fb
Emmanuel Jacob <eymarnweke@Emmanuels-MacBook-Pro.local>
2022-10-15 23:41:41 +0100

    ci: fix build err

--- store/index.js
+++ store/index.js
@@ -1,16 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 // import thunk from 'redux-thunk';
 import authReducer from './reducers/auth_reducer';
-import conversation_reducer from './reducers/conversation_reducer';
 import mediaReducer from './reducers/media_reducer';
-import messenger_reducer from './reducers/messenger_reducer';
 
 const reducers = combineReducers({
   auth: authReducer,
   media: mediaReducer,
-    messenger: messenger_reducer,
-  conversation: conversation_reducer
-
 });
 
 const store = configureStore({
