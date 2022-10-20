9921a50f665abb3d522b0b93e39b0d5795dee0b9
Stephen Ngwu <ngwustephen99@gmail.com>
2022-10-20 12:01:12 +0100

    wip: grouped notifications by date

--- components/notification/index.js
+++ components/notification/index.js
@@ -1,12 +1,47 @@
+import moment from 'moment';
+import { useRouter } from 'next/router';
 import { NotificationPageContainer } from '../../styles/notification.styles';
 import { Text2XL, TextLG } from '../utils/typography/Typography';
 import NotificationCard from './NotificationCard';
 
-const Notification = () => {
-  // const router = useRouter();
-  // const user = useSelector((state) => state.auth.currentUser);
-  // const notificationsUrl = `${API_URL}/notifications/${user?.slug}?page=${0}`;
-  // const { data: notifications } = useSWR(notificationsUrl);
+const Notification = ({ notifications }) => {
+  const router = useRouter();
+
+  const filteredNotifications = notifications?.sort((a, b) => {
+    return new Date(b.createdAt) - new Date(a.createdAt);
+  });
+
+  const groupedNotifications = filteredNotifications.reduce(
+    (acc, notification) => {
+      const date = moment(notification?.createdAt).calendar(null, {
+        lastDay: '[Yesterday]',
+        sameDay: '[Today]',
+        nextDay: '[Tomorrow]',
+        lastWeek: '[last] dddd',
+        nextWeek: 'dddd',
+        sameElse: 'ddd, L',
+      });
+
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+
+      acc[date].push(notification);
+      return acc;
+    },
+    {}
+  );
+
+  const dateSortedNotifications =
+    groupedNotifications !== undefined &&
+    Object?.keys(groupedNotifications)?.map((date) => {
+      return {
+        date: date,
+        notifications: groupedNotifications[date],
+      };
+    });
+
+  console.log('date sorted', dateSortedNotifications);
 
   return (
     <NotificationPageContainer>
