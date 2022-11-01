adced7d3c1d629a70aee62aeac7cd944371ad682
CodexpathCommunity <codexpath3@gmail.com>
2022-11-01 19:42:43 +0100

    style: home scroll

--- styles/home.styles.js
+++ styles/home.styles.js
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 export const HomePageContainer = styled.div`
   height: 100%;
   width: 100%;
-  overflow: hidden;
   display: flex;
 `;
 export const ContentPageContainer = styled.div`
   height: 100%;
   width: 100%;
-  overflow: scroll;
+  overflow-y: scroll;
   scroll-snap-type: y mandatory;
 `;
