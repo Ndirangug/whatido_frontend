b4aa373535cd137ef862bb457aba6fea9989f600
CodexpathCommunity <codexpath3@gmail.com>
2022-11-04 15:18:15 +0100

    wip: messegner

--- components/create/DragDrop.js
+++ components/create/DragDrop.js
@@ -41,25 +41,19 @@ function DragDrop({ setScreenShots, setSelectedSS }) {
       // setToken(tok);
       axios
         .post('http://localhost:4000/upload', videoFormData, {
-          onUploadProgress: (data) => {
-            setUploadPercent(Math.round((data.loaded / data.total) * 100));
-          },
-          cancelToken: new axios.CancelToken((src) => (tokenSrc.current = src)),
+          'Content-Type': 'multipart/form-data',
         })
         .then((response) => {
           setLoading(false);
-          {
-            response?.data?.data?.imageUrls.map((i) =>
-              console.log(`http://localhost:4000${i}`, 'image data')
-            );
-          }
+          console.log(response);
+
           // console.log(response?.data?.data?.imageUrls, 'upload reponse');
-          setVideoData(response?.data?.data);
-          setScreenShots(response?.data?.data?.imageUrls);
-          setSelectedSS(response?.data?.data?.imageUrls[0]);
-          setUploadStatus('videoUploaded');
-          setLoading(false);
-          setUploadPercent(null);
+          // setVideoData(response?.data?.data);
+          // setScreenShots(response?.data?.data?.imageUrls);
+          // setSelectedSS(response?.data?.data?.imageUrls[0]);
+          // setUploadStatus('videoUploaded');
+          // setLoading(false);
+          // setUploadPercent(null);
           // console.log('video uploaded successfully');
         })
         .catch((err) => {
---
+++ pages/messenger/ongoing/[id].js
@@ -0,0 +1,5 @@
+function Ongoing() {
+  return <div>Ongoing conversations</div>;
+}
+
+export default Ongoing;
