1488ff18b6eaffd2e5e01e7e17ad8c9da8f50c6b
CodexpathCommunity <codexpath3@gmail.com>
2022-11-11 13:14:38 +0100

    develop: about page desktop

--- styles/about.styles.js
+++ styles/about.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const AboutPageContainer = styled.div`
   padding: 2rem 1rem;
-  max-width: 1024px;
+  max-width: 1180px;
   margin: 0 auto;
   width: 100%;
   display: grid;
@@ -12,7 +12,20 @@ export const AboutPageContainer = styled.div`
 export const AboutCardContainer = styled.div`
   background: white;
   border-radius: 1rem;
+  flex-direction: ${({ row }) => (row ? 'row' : 'row-reverse')};
   display: flex;
   padding: 1rem;
   gap: 1rem;
+
+  .svg-container {
+    flex: 0.5;
+  }
+
+  .info-container {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+  }
 `;
