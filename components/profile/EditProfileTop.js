539b1ad2b41990864a315faa937dc9757c780f9e
CodexpathCommunity <codexpath3@gmail.com>
2022-11-15 20:50:34 +0100

    chore: edit profile

--- components/profile/EditProfileTop.js
+++ components/profile/EditProfileTop.js
@@ -1,5 +1,6 @@
 import { EditProfileTopContainer } from '../../styles/profile.styles';
 import LgAvatar from '../utils/avatars/LgAvatar';
+import UploadProfilePhotoIcon from '../utils/icons/UploadProfilePhotoIcon';
 
 function EditProfileTop() {
   return (
@@ -7,6 +8,7 @@ function EditProfileTop() {
       <div className="banner-container"></div>
       <div className="avatar-conatiner">
         <LgAvatar />
+        <UploadProfilePhotoIcon color />
       </div>
     </EditProfileTopContainer>
   );
