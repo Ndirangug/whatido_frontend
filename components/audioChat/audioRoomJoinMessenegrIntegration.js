1f3a59e61e6238cf0a6f62e06ebc9141a9f55345
George Ndirangu <ndirangu.mepawa@gmail.com>
2022-12-08 11:12:00 +0000

    chore: update styles

--- components/audioChat/audioRoomJoinMessenegrIntegration.js
+++ components/audioChat/audioRoomJoinMessenegrIntegration.js
@@ -1,56 +1,56 @@
-import { useState } from "react";
-import { joinRoom, recreatePeer } from "../../webRTC/peerEvents";
-import axios from "axios";
-import { AUDIOROOM_API_URL } from "../../constants/api";
-import { toast } from "react-toastify";
-import "./audioChat.css";
+import axios from 'axios';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { AUDIOROOM_API_URL } from '../../constants/api';
+import { joinRoom, recreatePeer } from '../../webRTC/peerEvents';
+import styles from './audioChat.module.css';
 
 export function AudioRoomJoinButton({ roomId, onJoined }) {
   const [loading, setLoading] = useState(false);
   return (
     <button
-      className={{styles['audio-room-join-button']}}
+      className={styles['audio-room-join-button']}
       onClick={() => {
         setLoading(true);
 
         axios
           .get(`${AUDIOROOM_API_URL}/rooms/${roomId}`)
           .then((response) => {
-            console.log("successfully fetched room", response.data);
+            console.log('successfully fetched room', response.data);
             const room = response.data;
             //fetch user role in room
             if (room.isLive) {
               recreatePeer(() => {
-                joinRoom(response.data, "Audience");
+                joinRoom(response.data, 'Audience');
                 onJoined();
                 setLoading(false);
               });
             } else {
-              toast.warn("Room expired");
+              toast.warn('Room expired');
             }
           })
           .catch((err) => {
             setLoading(false);
             if (err.response) {
               toast.error(err.response.data.message);
-              console.error("Couldnt fetch room", err.response.data.message);
+              console.error('Couldnt fetch room', err.response.data.message);
             } else {
-              toast.error("Couldnt fetch room");
-              console.error("Couldnt fetch room", err);
+              toast.error('Couldnt fetch room');
+              console.error('Couldnt fetch room', err);
             }
           });
       }}
     >
-      {loading ? "...JOINING" : "JOIN AUDIO ROOM"}
+      {loading ? '...JOINING' : 'JOIN AUDIO ROOM'}
     </button>
   );
 }
 
 export function useAudioRoomLinkParser(message) {
   //if message contains a audio room id
-  if (message.includes("#AUDIOROOMID")) {
+  if (message.includes('#AUDIOROOMID')) {
     //extract the link
-    const [messageText, roomId] = message.split("#AUDIOROOMID");
+    const [messageText, roomId] = message.split('#AUDIOROOMID');
     console.log(`extracted audio room id ${roomId} and text ${messageText}`);
     return { messageText, roomId };
   }
